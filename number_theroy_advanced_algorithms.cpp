1098c1929b17309b6b6fb9aebb6866a6968a57c8
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-07-26 19:54:34 +0300

    Create number_theory_advanced_algorithms.cpp
    Initial implementation of advanced number theory algorithms.

---
+++ number_theroy_advanced_algorithms.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define int long long
+
+
+// a%b = a-(floor(a/b))      because we still substract by b till value is smaller than a
+
+
+
+
+
+signed main (){
+
+
+
+
+}
