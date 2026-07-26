71e45a208c743a4ce9a2582e3ef54fd128837503
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-07-26 20:01:04 +0300

    Enhance gcd explanation in number theory algorithms
    Added comments explaining the properties of gcd.

--- math/number_theory/number_theroy_advanced_algorithms.cpp
+++ math/number_theory/number_theroy_advanced_algorithms.cpp
@@ -7,6 +7,20 @@ using namespace std;
 
 // a%b = a-(floor(a/b))      because we still substract by b till value is smaller than a
 
+// we know that gcd (a,b) = gcd(b,a)
+
+//gcd(b,a)=gcd(b%a,a)              here is why  >====
+//and since gcd(a,b)=gcd(b,a)                        |
+//then gcd(a,b)=gcd(b%a,a)                           |
+                                                       //reason: 
+                                                              // g is gcd(a,b)
+                                                              // a/g is integer and b/g is integer then a/g-b/g is integer which means that a-b is divisible by g also
+                                                              // so substracting by the another value doesn't change the answer and this is the proof
+
+
+
+
+
 
 
 
