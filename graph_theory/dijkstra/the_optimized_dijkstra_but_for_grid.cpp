c05045638b722cdd4c8e10c6e04fa595e37f9c54
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-08-13 19:39:20 +0300

    Refactor Dijkstra's algorithm for grid with long long

--- graph_theory/dijkstra/the_optimized_dijkstra_but_for_grid.cpp
+++ graph_theory/dijkstra/the_optimized_dijkstra_but_for_grid.cpp
@@ -4,24 +4,30 @@ using namespace std;
 // bool multicases_=true;
 bool multicases_=false;
 
-#define int long long
+////////////////////////////
+//#define int long long
 
 struct point {
 	int x,y;
 };
 
 struct node{
-	int x,y,cost;//
+	int x,y;
+	long long cost;//
 };
+
+
 
 
 
 vector<int>dx = {-1, 1, 0, 0};
 vector<int>dy = {0, 0, -1, 1};
-/*
-vector<int> dx = {-1, 1, 0, 0, -1, -1, 1, 1};
-vector<int> dy = {0, 0, -1, 1, -1, 1, -1, 1};
 
+
+/*
+//vector<int> dx = {-1, 1, 0, 0, -1, -1, 1, 1};
+vector<int> dy = {0, 0, -1, 1, -1, 1, -1, 1};
+*/
 /*
 //if diagonal has extra cost:
 const double diagonal_bonus = sqrt(2.0); // ~1.41421356
@@ -31,13 +37,12 @@ vector<int> diagonal_cost = {0, 0, 0, 0, diagonal_bonus, diagonal_bonus, diagona
 
 
 
-*/
 
 
 
 
 struct cmp{
-	bool operator()(node&a,node&b){
+	bool operator()(const node&a,const node&b)const{
 		return a.cost>b.cost;
 	}
 };
@@ -51,22 +56,22 @@ struct cmp{
 
 
 
-
+/*
 //////////////////////////////////////////////////////////////////take care here : what is the maximum rows and columns????????
 	///is it good or some test cases with larger rows and fewer columns (and vice versa) ?????????
 int Nx=5e3+5;
 int Ny=5e3+5;
 vector<vector<int>>grid(Nx,vector<int>(Ny,0));
 //vector<bool>vis(N,false);
-vector<vector<point>>parent(Nx,vector<point>(Ny,{-1,-1});
+vector<vector<point>>parent(Nx,vector<point>(Ny,{-1,-1}) );
 
-vector<vector<int>>dis(Nx,vector<int>(Ny,1e18));
+vector<vector<long long>>dis(Nx,vector<long long>(Ny,1e18));
+*/
 
-/*
 vector<vector<int>> grid;
 vector<vector<point>> parent;
-vector<vector<int>> dis;
-*/
+vector<vector<long long>> dis;
+
 
 
 
@@ -80,30 +85,31 @@ bool isvalid(int row, int col) {
 	return (row >= 1 && row <= R && col >= 1 && col <= C);
 }
 
-int dijkstra (point src, point target){
+long long dijkstra (point src, point target){
 	priority_queue<node,vector<node>,cmp>pq;
 	
-	dis[src.x][src.y]=0;////////////////
+	dis[src.x][src.y]=0;////////////////  
 	pq.push({src.x,src.y,0});
 	
 	while(!pq.empty()){
 		node u = pq.top();
 		pq.pop();
-
-		if(u.cost > dis[u.x][u.y]) continue;
 		
-		if(u.x==target.x&&u.y==target.y)return u.cost;
+		
+		if(u.cost > dis[u.x][u.y]) continue;	
+		
+		if(u.x==target.x&&u.y==target.y)return u.cost;	
 		
 		//if(vis[u.idx]) continue;
 		//vis[u.idx]=true;
 		
-		for(int i = 0 ;i < (int)dx.size ; i ++){
+		for(int i = 0 ;i < (int)dx.size() ; i ++){
 			int nx=u.x+dx[i];
 			int ny=u.y+dy[i];
 
 			if(isvalid(nx,ny)){
-				int edgeCost = grid[nx][ny];
-				int newDist = u.cost + edgeCost;
+				long long edgeCost = grid[nx][ny];
+				long long newDist = u.cost + edgeCost;
 				
 				//////// if there  is a diagonal cost if 8 movement directions allowed 
 				// int edgeCost = grid[nx][ny] + diagonal_cost[i];
@@ -120,7 +126,7 @@ int dijkstra (point src, point target){
 		
 	}
 	
-	return -1;
+	return -1LL;
 }
 
 
@@ -142,7 +148,7 @@ void grid_clear(int R,int C){
 void grid_resize_and_clear(int rows, int cols) {
 	// Size dynamically up to rows + 1 / cols + 1 to perfectly handle 1-based indexing
 	grid.assign(rows + 1, vector<int>(cols + 1, 0));
-	dis.assign(rows + 1, vector<int>(cols + 1, 1e18));
+	dis.assign(rows + 1, vector<long long>(cols + 1, 1e18));
 	parent.assign(rows + 1, vector<point>(cols + 1, {-1, -1}));
 }
 
@@ -161,13 +167,13 @@ void solve(int tc){
 	 // cerr<<"at the test case no."<<tc<<" : \n";
 	
 	// cin>>n>>m>>src>>target;
-	cin>>R>>C; 
+	cin>>R>>C;
 	
 	
 	
 	///////////which one is used???mmm..
-	grid_clear(R,C);
-	// grid_resize_and_clear(R,C); 
+	// grid_clear(R,C);
+	grid_resize_and_clear(R,C); 
 	
 	
 	
@@ -183,7 +189,7 @@ void solve(int tc){
 		}
 	}
 	
-	int ans = dijkstra(src,target);
+	long long ans = dijkstra(src,target);
 	
 	if(ans==-1){
 		
@@ -196,7 +202,7 @@ void solve(int tc){
 		
 		vector<point>path;
 		point cur=target;
-		while(cur.x != -1 && cur.y != -1) {     //while(cur!={-1,-1}){ //< this is not correct unless operators define for != with the struct
+		while(cur.x != -1 && cur.y != -1){    //while(cur!={-1,-1}){ //< this is not correct unless operators define for != with the struct
 			path.push_back(cur);
 			cur=parent[cur.x][cur.y];
 		}
