6427553b8b612af96323509f2ffcbb1fa1463007
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-08-14 10:03:59 +0300

    Add comments explaining PBDS functions
    Added explanation for PBDS functions in comments.

--- range_queries/ordered_sets_and_mulitsets.cpp
+++ range_queries/ordered_sets_and_mulitsets.cpp
@@ -9,6 +9,7 @@ using namespace __gnu_pbds;
 template<class  T>using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag,tree_order_statistics_node_update>;
 template<typename T>using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 /*
+explanation from ai:
     Two important PBDS functions:
 
     1) s.order_of_key(x)
