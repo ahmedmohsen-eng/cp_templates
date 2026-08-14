f61bd747a22b2506f58626c2e9d6fb5f0432cb15
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-08-14 10:03:41 +0300

    Document PBDS functions in ordered_sets_and_mulitsets.cpp
    Added explanations for PBDS functions order_of_key and find_by_order.

--- range_queries/ordered_sets_and_mulitsets.cpp
+++ range_queries/ordered_sets_and_mulitsets.cpp
@@ -8,24 +8,45 @@ using namespace std;
 using namespace __gnu_pbds;
 template<class  T>using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag,tree_order_statistics_node_update>;
 template<typename T>using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
-
 /*
-5. The two important functions
+    Two important PBDS functions:
 
-These are the main reason to use PBDS:
+    1) s.order_of_key(x)
 
-s.order_of_key(x);
+       Returns the number of elements strictly less than x.
 
-and
+       For example:
+       S = {10, 20, 30, 40, 50}
 
-s.find_by_order(k);
-order_of_key
+       s.order_of_key(30) = 2
+       because {10, 20} are strictly less than 30.
 
-Returns:
+       If the keys are integers and you want the number of elements <= x:
 
-#{a∈S:a<x}
-//////////////////////notice less only not less or equal !!      so if you want elements less or equal use +1 
+       s.order_of_key(x + 1);
 
-/////////****important
-/////////////////////find_by_order(k)​ starts from index 0 !!!!!
+
+    2) s.find_by_order(k)
+
+       Returns an iterator to the element at 0-based position k.
+
+       For example:
+       S = {10, 20, 30, 40, 50}
+
+       *s.find_by_order(0) = 10
+       *s.find_by_order(1) = 20
+       *s.find_by_order(2) = 30
+       *s.find_by_order(3) = 40
+       *s.find_by_order(4) = 50
+
+       Therefore, if you want the k-th smallest element using
+       1-based indexing:
+
+       *s.find_by_order(k - 1)
+
+       IMPORTANT:
+       +1 is relevant to order_of_key when converting < x to <= x
+       (for integer keys).
+
+       +1 is NOT how you convert find_by_order to 1-based indexing.
 */
