2e782453167e42af01bce95156549820dadc1c10
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-08-17 07:59:42 +0300

    Refactor ordered_multiset to use pairs
    Updated the definition of ordered_multiset to store pairs of elements and their counts.

--- range_queries/ordered_sets_and_mulitsets.cpp
+++ range_queries/ordered_sets_and_mulitsets.cpp
@@ -6,7 +6,8 @@ using namespace std;
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
 using namespace __gnu_pbds;
-template<class  T>using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag,tree_order_statistics_node_update>;
+// template<class  T>using ordered_multiset = tree<T,null_type,less_equal<T>,rb_tree_tag,tree_order_statistics_node_update>;
+template<typename T>using ordered_multiset = tree<pair<T, int>, null_type, less<pair<T, int>>, rb_tree_tag, tree_order_statistics_node_update>;
 template<typename T>using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>;
 /*
 explanation from ai:
