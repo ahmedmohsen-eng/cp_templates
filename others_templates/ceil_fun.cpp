85852bbc5c695e9800efa8e12e1dda5277946a2f
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-09-06 20:42:29 +0300

    Fix include directive and redefine int as ll

--- others_templates/ceil_fun.cpp
+++ others_templates/ceil_fun.cpp
@@ -1,7 +1,7 @@
-#including<bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define int long long
+#define ll long long
 
 inline ll ceil0(ll a, ll b) {
     return a / b + ((a ^ b) > 0 && a % b);
