4a3f1141395944e60fc88ee98e507f31e039b97f
Ahmed Mohsen <97984942+ahmedmohsen-eng@users.noreply.github.com>
2026-09-24 10:47:47 +0300

    Enhance comments on BIT usage tricks
    Added comments regarding tricks for using BIT with min operations and suffix queries.

--- data_structures_and_range_queries/BIT__or__(Fenwick_tree).cpp
+++ data_structures_and_range_queries/BIT__or__(Fenwick_tree).cpp
@@ -211,7 +211,12 @@ struct BIT {
 
 
 
+//////////trick:
+//if you want min, and other operations like that :
+//you can use them but with point query not range query
 
+///////////trick:
+///if you want suffix not prefix then reverse the array and then build the BIT
 
 
 
